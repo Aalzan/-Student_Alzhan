fe3cc21068317989fcfab9a2210fb7069f4e11be
Alzhan <149990772+Aalzan@users.noreply.github.com>
2023-11-15 15:04:10 +0600

    Add files via upload

---
+++ react.js
@@ -0,0 +1,43 @@
+function Hello(props) {
+    return (
+      <div>
+        <p>Имя: {props.name}</p>
+        <p>Возраст: {props.age}</p>
+      </div>
+    );
+  }
+
+  ReactDOM.render(
+    <Hello name="Альжан" age={17} />,
+    document.getElementById("app")
+  );
+
+class ClickButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        class: "off",
+        label: "Нажми"
+      };
+      this.press = this.press.bind(this);
+    }
+  
+    press() {
+      let className = this.state.class === "off" ? "on" : "off";
+      this.setState({ class: className });
+    }
+  
+    render() {
+      return (
+        <button onClick={this.press} className={this.state.class}>
+          {this.state.label}
+        </button>
+      );
+    }
+  }
+
+  ReactDOM.render(
+    <ClickButton />,
+    document.getElementById("app")
+  );
+  
